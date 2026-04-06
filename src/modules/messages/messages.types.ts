5b064dede2290420cc50ced55700df507ec38c47
mgalen007 <hyllusx@gmail.com>
2026-04-06 21:44:59 +0200

    Added sessionID link to messages

--- src/modules/messages/messages.types.ts
+++ src/modules/messages/messages.types.ts
@@ -3,5 +3,6 @@ import { Types, Document } from "mongoose"
 export interface IMessage extends Document {
     sender: Types.ObjectId,
     recipient: Types.ObjectId,
+    sessionID: Types.ObjectId,
     content: string
 }
